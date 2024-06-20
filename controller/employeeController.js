3e5d4bfc6c8ec5b14adfc37c171b6098e822ba78
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-06-20 16:20:27 +0530

    Feat : Created the UpdateEmployee Controller

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,7 +1,6 @@
 const Emp = require("../model/employeeModel");
 const jwt = require("jsonwebtoken");
 
-
 const createEmployeeController = async(req,res) =>{
     try {
         const user = req.user;
@@ -24,5 +23,27 @@ const createEmployeeController = async(req,res) =>{
     }
 }
 
+const updateEmployeeController = async(req,res)=>{
+    try {
+        const user = req.user;
 
-module.exports = {createEmployeeController};
+        if(user.role !== "admin") return res.status(401).json({Message:"UnAuthorised User"});
+
+        const {name,email,Role} = req.body;
+        const id = req.params.id;
+
+        if(!id) return res.status(404).json("No Employee found");
+
+        const updateDetails = await Emp.findByIdAndUpdate(id,{name,email,Role});
+        if(!updateDetails) return res.status(404).json({Message:"Not found"});
+
+        res.status(200).json(updateDetails);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json("Internal server error");
+    }
+}
+
+
+module.exports = {createEmployeeController,updateEmployeeController};
--- routes/employee.js
+++ routes/employee.js
@@ -1,11 +1,14 @@
 const express = require("express"); 
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
-const { createEmployeeController } = require("../controller/employeeController");
+const { createEmployeeController, updateEmployeeController } = require("../controller/employeeController");
 
 router.use(authMiddleware);
 
-router.post("/",createEmployeeController);
+router.post("/create",createEmployeeController);
+
+router.put("/update/:id",updateEmployeeController);
+
 
 module.exports = router;
 
