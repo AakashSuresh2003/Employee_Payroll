89d2006b240adeb9e64b00781e9a7f09c26d6f4d
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-06-22 11:13:44 +0530

    Deployment Modification commit

--- index.js
+++ index.js
@@ -1,64 +1,61 @@
-const dotenv = require('dotenv').config();
-const express = require('express');
-const ConnectDB = require('./db/database');
+const dotenv = require("dotenv").config();
+const express = require("express");
+const ConnectDB = require("./db/database");
 const app = express();
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth");
 const employeeRouter = require("./routes/employee");
 const hrRouter = require("./routes/hr");
 const faRouter = require("./routes/finance");
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-
 const swaggerOptions = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
-        info: {
-          title: 'Payroll API',
-          description: 'API endpoints for payroll services documented on Swagger',
-          contact: {
-            name: 'Aakash S',
-            email: 'aakashsuresh455@gmail.com',
-            url: 'https://github.com/aakashsuresh2003/Employee_Payroll',
-          },
-          version: '1.0.0',
-        },
-      servers: [
-        {
-            url: 'https://employee-payroll-jet.vercel.app',
-          description: 'Local server'
-        }
-      ]
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Payroll API",
+      description: "API endpoints for payroll services documented on Swagger",
+      contact: {
+        name: "Aakash S",
+        email: "aakashsuresh455@gmail.com",
+        url: "https://github.com/aakashsuresh2003/Employee_Payroll",
+      },
+      version: "1.0.0",
     },
-    apis: ['./routes/*.js'],
-  };
-  
+    servers: [
+      {
+        url: "https://employee-payroll-jet.vercel.app",
+        description: "Deployed server",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.get("/",(req,res)=>{
-    res.status(200).json("Welcome to Employee Payroll API");
-})
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome to Employee Payroll API");
+});
 
-
-app.use("/api/v1/auth",authRouter);
-app.use("/api/v1/employee",employeeRouter);
-app.use("/api/v1/hr",hrRouter);
-app.use("/api/v1/fa",faRouter);
-
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/employee", employeeRouter);
+app.use("/api/v1/hr", hrRouter);
+app.use("/api/v1/fa", faRouter);
 
 ConnectDB();
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
-})
+  console.log("Server is running on port 3000");
+});
