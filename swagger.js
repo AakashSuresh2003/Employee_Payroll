a19a4d15a5224670e42ef65ec32a00c05043d498
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-06-22 09:14:46 +0530

    Deployment modifications

--- swagger.js
+++ swagger.js
@@ -1,38 +1,37 @@
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const options = {
   definition: {
-    openapi: "3.0.0",
+    openapi: '3.0.0',
     info: {
-      title: "Payroll API",
-      description: "API endpoints for payroll services documented on Swagger",
+      title: 'Payroll API',
+      description: 'API endpoints for payroll services documented on Swagger',
       contact: {
-        name: "Aakash S",
-        email: "aakashsuresh455@gmail.com",
-        url: "https://github.com/aakashsuresh2003/Employee_Payroll",
+        name: 'Aakash S',
+        email: 'aakashsuresh455@gmail.com',
+        url: 'https://github.com/aakashsuresh2003/Employee_Payroll',
       },
-      version: "1.0.0",
+      version: '1.0.0',
     },
     servers: [
       {
-        url: "https://employee-payroll-black.vercel.app",
-        description: "Vercel server",
-      }
+        url: 'https://employee-payroll-black.vercel.app',
+        description: 'Vercel server',
+      },
     ],
   },
-  apis: ["./routes/*.js"], 
+  apis: ['./routes/*.js'],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
 const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
 
-  
 function swaggerDocs(app, port) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec , {customCssUrl: CSS_URL }));
-  app.get("/docs.json", (req, res) => {
-    res.setHeader("Content-Type", "application/json");
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL }));
+  app.get('/docs.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
     res.send(swaggerSpec);
   });
 }
