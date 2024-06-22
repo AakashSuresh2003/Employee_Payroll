020ca13e2103bc6d02548e4183350c7bcb46958b
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-06-22 09:30:17 +0530

    Deployment modifications

--- swagger.js
+++ swagger.js
@@ -1,7 +1,6 @@
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const express = require('express');
-const path = require('path');
+
 
 const options = {
   definition: {
@@ -29,9 +28,7 @@ const options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 function swaggerDocs(app, port) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
-  }));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   
   app.get('/docs.json', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
