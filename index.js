8669ea6e6f3fb004c843e97add9fd321ab42bda8
Aakash <aakashsuresh455@gmail.com>
2024-08-11 13:35:10 +0530

    Modified

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(cors({origin:true,credentials:true}));
 
 app.get("/", (req, res) => {
-  res.status(200).json("Welcome to Employee Payroll API v5");
+  res.status(200).json("Welcome to Employee Payroll API v6");
 });
 
 app.use("/api/v1/auth", authRouter);
